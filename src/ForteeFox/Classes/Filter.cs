0fb62f0acdeab3c055e45f8f30c488a9b17c4b24
John Fox Maule <john@fox-maule.dk>
2024-01-07 16:42:06 +0100

    Initial commit

---
+++ src/ForteeFox/Classes/Filter.cs
@@ -0,0 +1,9 @@
+using System;
+namespace ForteeFox {
+
+    public class Filter {
+        public string source { get; set; }
+        public string condition { get; set; }
+        public string[] target { get; set; }        
+    }
+}
